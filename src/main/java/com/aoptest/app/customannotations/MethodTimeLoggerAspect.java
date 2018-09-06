2bc5de4b2d42a328f69040f5c42828ea2790570f
Aashish Pundalik <pundalikaashish@gmail.com>
2018-09-06 10:36:22 +0800

    Tests for the Custom annotation.

--- src/main/java/com/aoptest/app/customannotations/MethodTimeLoggerAspect.java
+++ src/main/java/com/aoptest/app/customannotations/MethodTimeLoggerAspect.java
@@ -1,17 +1,17 @@
 package com.aoptest.app.customannotations;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MethodTimeLoggerAspect {
 
-    private static final Logger LOGGER = LogManager.getLogger();
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodTimeLoggerAspect.class);
 
     @Around("@annotation(com.aoptest.app.customannotations.MethodTimeLogger)")
     public Object methodTimeLogger(ProceedingJoinPoint joinPoint) throws Throwable {
@@ -21,7 +21,7 @@ public class MethodTimeLoggerAspect {
 
         long timeTaken = System.currentTimeMillis() - startTime;
 
-        LOGGER.info("The method " + joinPoint.getSignature() + "took " + timeTaken + "ms");
+        LOGGER.info("The method " + joinPoint.getSignature() + " took " + timeTaken + "ms");
         return proceed;
     }
 }
--- src/test/java/com/aoptest/app/customannotations/AspectTestStrategyIntegration.java
+++ src/test/java/com/aoptest/app/customannotations/AspectTestStrategyIntegration.java
@@ -2,25 +2,38 @@ package com.aoptest.app.customannotations;
 
 
 import com.aoptest.app.AOPUnitTesting;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Method;
+
+import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.mockStatic;
 import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PowerMockRunnerDelegate(SpringRunner.class)
 @PowerMockIgnore({"javax.management.*", "javax.script.*"})
-@PrepareForTest(MethodTimeLoggerAspect.class)
+@PrepareForTest({MethodTimeLoggerAspect.class, LoggerFactory.class})
 @ContextConfiguration(classes = AOPUnitTesting.class,
         initializers = ConfigFileApplicationContextInitializer.class)
 public class AspectTestStrategyIntegration {
@@ -28,12 +41,39 @@ public class AspectTestStrategyIntegration {
     @Autowired
     private CustomAnnotationTestDelegate customAnnotationTestDelegate;
 
+    private static Logger loggerMock;
+
+    @BeforeClass
+    public static void classSetup() {
+        loggerMock = mock(Logger.class);
+        mockStatic(System.class);
+        mockStatic(LoggerFactory.class);
+
+        when(LoggerFactory.getLogger(MethodTimeLoggerAspect.class)).thenReturn(loggerMock);
+    }
+
     @Test
     public void shouldLogMethodTiming() {
-        mockStatic(System.class);
+        long startTime = 100L;
+        long endTime = 400L;
+        long expectedTimeTaken = endTime - startTime;
 
-        when(System.currentTimeMillis()).thenReturn(100L);
+        when(System.currentTimeMillis()).thenReturn(startTime, endTime);
 
         customAnnotationTestDelegate.methodWithCustomAnnotation();
+
+        Method method = ReflectionUtils.findMethod(CustomAnnotationTestDelegate.class,
+                "methodWithCustomAnnotation");
+
+        String methodSignature = new StringBuilder()
+                .append(method.getReturnType().toString())
+                .append(" ")
+                .append(CustomAnnotationTestDelegate.class.getCanonicalName())
+                .append(".")
+                .append(method.getName())
+                .append("()")
+                .toString();
+
+        Mockito.verify(loggerMock).info("The method "+methodSignature+" took "+expectedTimeTaken+"ms");
     }
 }
